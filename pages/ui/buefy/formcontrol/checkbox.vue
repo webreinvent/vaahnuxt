c8fd1ae03802b1e5f74ad6c120610004866e8311
Akshat Aggarwal <akshat.a001@webreinvent.com>
2020-01-11 16:38:28 +0530

    Checkbox Page Completed

---
+++ pages/ui/buefy/formcontrol/checkbox.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <div class="main-content">
+
+    <SectionTitle title="Buefy - Checkbox"
+                  link="https://buefy.org/documentation/checkbox/">
+    </SectionTitle>
+
+
+    <SectionCode name="Checkbox" :code="CheckboxCode">
+      <Checkbox></Checkbox>
+    </SectionCode>
+
+
+    <SectionCode name="Grouped (Array)" :code="GroupedCode">
+      <Grouped></Grouped>
+    </SectionCode>
+
+
+    <SectionCode name="Sizes" :code="SizesCode">
+      <Sizes></Sizes>
+    </SectionCode>
+
+
+    <SectionCode name="Types" :code="TypesCode">
+      <Types></Types>
+    </SectionCode>
+
+
+    <SectionCode name="Checkbox Button" :code="CheckboxButtonCode">
+      <CheckboxButton></CheckboxButton>
+    </SectionCode>
+
+
+  </div>
+
+</template>
+
+<script src="./checkboxjs.js"></script>
