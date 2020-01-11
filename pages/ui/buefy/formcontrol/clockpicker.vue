5797debc56510add04b5869685655cbe54056e91
Akshat Aggarwal <akshat.a001@webreinvent.com>
2020-01-11 17:27:20 +0530

    Clockpicker & datepicker page completed

---
+++ pages/ui/buefy/formcontrol/clockpicker.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <div class="main-content">
+
+    <SectionTitle title="Buefy - Clockpicker"
+                  link="https://buefy.org/documentation/checkbox/">
+    </SectionTitle>
+
+
+    <SectionCode name="Clockpicker" :code="ClockpickerCode">
+      <Clockpicker></Clockpicker>
+    </SectionCode>
+
+
+    <SectionCode name="Non Read Only" :code="NonReadOnlyCode">
+      <NonReadOnly></NonReadOnly>
+    </SectionCode>
+
+
+    <SectionCode name="Range" :code="RangeCode">
+      <Range></Range>
+    </SectionCode>
+
+
+    <SectionCode name="Footer" :code="FooterCode">
+      <Footer></Footer>
+    </SectionCode>
+
+
+    <SectionCode name="Colors" :code="ColorsCode">
+      <Colors></Colors>
+    </SectionCode>
+
+
+  </div>
+
+</template>
+
+<script src="./clockpicker.js"></script>
