2aee0804984a6a988cdc53c09f2e2844f14823ac
Akshat Aggarwal <akshat.a001@webreinvent.com>
2020-01-11 16:20:33 +0530

    AutoComplete Page Completed

---
+++ pages/ui/buefy/formcontrol/autocomplete.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <div class="main-content">
+
+    <SectionTitle title="Buefy - Autocomplete"
+                  link="https://buefy.org/documentation/autocomplete/">
+    </SectionTitle>
+
+
+    <SectionCode name="AutoComplete" :code="AutoCompleteCode">
+      <AutoComplete></AutoComplete>
+    </SectionCode>
+
+
+    <SectionCode name="Object Array" :code="ObjectArrayCode">
+      <ObjectArray></ObjectArray>
+    </SectionCode>
+
+
+    <SectionCode name="Header" :code="HeaderCode">
+      <Header></Header>
+    </SectionCode>
+
+
+    <SectionCode name="Footer" :code="FooterCode">
+      <Footer></Footer>
+    </SectionCode>
+
+
+    <SectionCode name="Async With Custom Template" :code="AsyncWithCustomTemplateCode">
+      <AsyncWithCustomTemplate></AsyncWithCustomTemplate>
+    </SectionCode>
+
+
+    <SectionCode name="Async With Infinite Scroll" :code="AsyncWithInfiniteScrollCode">
+      <AsyncWithInfiniteScroll></AsyncWithInfiniteScroll>
+    </SectionCode>
+
+
+  </div>
+
+</template>
+
+<script src="./autocompleteJs.js"></script>
