521825166d065c4735786580ef47a41a5ea6a858
Akshat Aggarwal <akshat.a001@webreinvent.com>
2020-01-13 17:32:21 +0530

    icons page completed

---
+++ pages/ui/buefy/icon.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <div class="main-content">
+
+    <SectionTitle title="Buefy - Icon"
+                  link="https://buefy.org/documentation/icon/">
+    </SectionTitle>
+
+
+    <SectionCode name=" Material Design Icons" :code="IconCode">
+      <Icon></Icon>
+    </SectionCode>
+
+
+    <SectionCode name="FontAwesome" :code="FontAwesomeCode">
+      <FontAwesome></FontAwesome>
+    </SectionCode>
+
+
+    <SectionCode name="Object Syntax" :code="ObjectSyntaxCode">
+      <ObjectSyntax></ObjectSyntax>
+    </SectionCode>
+
+
+    <SectionCode name="Custom Icon Pack" :code="CustomIconPackCode">
+      <b-notification
+        type="is-warning"
+        aria-close-label="Close notification"
+        role="alert">
+        Custom Icon Pack Generating errors and needs to be fixed.
+      </b-notification>
+      <!--<CustomIconPack></CustomIconPack>-->
+    </SectionCode>
+
+
+  </div>
+
+</template>
+
+<script src="./iconjs.js"></script>
