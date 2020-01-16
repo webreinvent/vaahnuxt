823628c9c5384c63e40dc994ec132b0ded258130
Akshat Aggarwal <akshat.a001@webreinvent.com>
2020-01-16 20:03:11 +0530

    Update

--- ui/blocks/ServicesBigImageWithBox.vue
+++ ui/blocks/ServicesBigImageWithBox.vue
@@ -12,7 +12,7 @@
           <div class="column is-10 is-offset-2"><img :src="service.src" class="image-fluid" alt="wrappixel" /></div>
           <div class="column is-5-desktop is-7-tablet has-text-centered wrap-feature30-box" data-aos="fade-left" data-aos-duration="1800" data-aos-easing="linear">
             <div class="card card-shadow">
-              <div class="card-body">
+              <div class="card-content">
                 <div class="p-20">
                   <h3 class="title is-3">{{service.heading}}</h3>
                   <p class="content">{{service.details}}</p>
@@ -26,7 +26,7 @@
           <div class="column is-10 feature_image"><img :src="service.src" class="image-fluid" alt="wrappixel" /></div>
           <div class="column is-5-desktop is-7-tablet has-text-centered wrap-feature30-box1" data-aos="fade-left" data-aos-duration="1800" data-aos-easing="linear">
             <div class="card card-shadow">
-              <div class="card-body">
+              <div class="card-content">
                 <div>
                   <h3 class="title is-3">{{service.heading}}</h3>
                   <p class="content">{{service.details}}</p>
