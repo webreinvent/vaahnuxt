f4c02991df88683ac7ee7e13cb527ac1d2676178
Pradeep <webreinvent@gmail.com>
2020-01-17 20:41:13 +0530

    home page

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,36 +3,9 @@
 
     <TopMenu></TopMenu>
 
-    <div class="container" >
+    <nuxt />
 
-      <section class="main-content columns">
-        <aside class="column is-2 section">
-          <p class="menu-label is-hidden-touch">
-            General
-          </p>
-          <ul class="menu-list">
-            <li
-              v-for="(item, key) of items"
-              :key="key"
-            >
-              <nuxt-link
-                :to="item.to"
-                exact-active-class="is-active"
-              >
-                <b-icon :icon="item.icon" /> {{ item.title }}
-              </nuxt-link>
-            </li>
-          </ul>
-        </aside>
-
-        <div class="container column is-10">
-          <nuxt />
-        </div>
-      </section>
-
-      <UIFooter></UIFooter>
-
-    </div>
+    <UIFooter></UIFooter>
 
   </div>
 </template>
@@ -50,18 +23,7 @@ export default {
 
     data () {
       return {
-        items: [
-          {
-            title: 'Home',
-            icon: 'home',
-            to: { name: 'index' }
-          },
-          {
-            title: 'Inspire',
-            icon: 'lightbulb',
-            to: { name: 'inspire' }
-          }
-        ]
+
       }
     }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,23 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
 
-      <h1>NuxtJs Boilerplate</h1>
+  <section class="section is-fullwidth" >
 
-    </div>
+      <section class="hero is-medium is-primary is-bold">
+        <div class="hero-body">
+          <div class="container has-text-centered">
+            <h1 class="title">
+              VaahNuxt
+            </h1>
+            <h2 class="subtitle">
+              Open Source Nuxt Boilerplate ready for your next web project.
+            </h2>
+          </div>
+        </div>
+      </section>
+
   </section>
+
+
 </template>
 
 <script>
