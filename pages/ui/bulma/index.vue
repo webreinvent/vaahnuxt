bd38235e60406e9959c6d5846b2af9f2a5d71e34
Akshat Aggarwal <akshat.a001@webreinvent.com>
2020-01-31 16:01:46 +0530

    Bulma page init

---
+++ pages/ui/bulma/index.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <div>
+
+    <Block></Block>
+
+  </div>
+
+</template>
+
+<script>
+  import Block from '../../../ui/docs/pages/index'
+
+  export default {
+    layout: 'ui',
+    head () {
+      return {
+        title: 'Buefy UI Docs',
+      }
+    },
+    components: {
+      Block
+    },
+    methods:{
+    },
+
+  }
+
+</script>
